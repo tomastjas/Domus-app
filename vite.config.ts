58d0244382bb8f3ec802e257e7acfc1a7a7e9875
tomastjas <tomasjefferson092@gmail.com>
2026-05-02 22:48:58 -0300

    Change base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
   },
   envDir: path.resolve(import.meta.dirname),
   root: path.resolve(import.meta.dirname, "client"),
-  base: "./",
+  base: "/Domus-app/",
   build: {
     outDir: path.resolve(import.meta.dirname, "dist"),
     emptyOutDir: true,
