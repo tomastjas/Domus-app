4766756797cd95d52b7d28f5394aeffa6abadc5d
tomastjas <tomasjefferson092@gmail.com>
2026-05-02 23:46:53 -0300

    Fix base path for Netlify

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
   },
   envDir: path.resolve(import.meta.dirname),
   root: path.resolve(import.meta.dirname, "client"),
-  base: "/Domus-app/",
+  base: "/",
   build: {
     outDir: path.resolve(import.meta.dirname, "dist"),
     emptyOutDir: true,
